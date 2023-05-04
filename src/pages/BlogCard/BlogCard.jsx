132ae69292ad41c5b6510985043e9a4759a97d7b
alrakib581 <alrakib581@gmail.com>
2023-05-04 12:17:58 +0600

    react to print packeg and code added

--- src/pages/BlogCard/BlogCard.jsx
+++ src/pages/BlogCard/BlogCard.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import ReactToPrint from 'react-to-print';
+import { useRef } from 'react';
 
 const BlogCard = ({blog}) => {
+    const ref = useRef()
     const {title, details} = blog;
     return (
         <div className='col-md-6'>
-            <div className='p-4'>
-                <h2>{title}</h2>
-                <p>{details}</p>
+            <div className=''>
+               <div ref={ref} className='p-4'>
+                    <h2>{title}</h2>
+                    <p>{details}</p>
+               </div>
+                <ReactToPrint trigger={()=> <button className='btn btn-outline-dark ms-4'>Print this blog</button>} content={()=> ref.current} />
             </div>
+               
         </div>
     );
 };
